eaac87518beff61f73daf08e828ca6fe16fd6b88
Lucas Reppe Welander <lucaswelanderx@gmail.com>
2021-12-11 11:44:25 +0100

    Added all competetive maps

--- src/Radar/maps/index.js
+++ src/Radar/maps/index.js
@@ -1,4 +1,12 @@
 import demirage from './de_mirage.png';
+import deinferno from './de_inferno.png';
+import deoverpass from './de_overpass.png';
+import denuke from './de_nuke.png';
+import deancient from './de_ancient.png';
+import devertigo from './de_vertigo.png';
+import detrain from './de_train.png';
+import dedust2 from './de_dust2.png';
+import decache from './de_cache.png';
 
 export default {
   de_mirage: {
@@ -6,5 +14,55 @@ export default {
     y: 1713,
     scale: 5.0,
     map: demirage,
+  },
+  de_inferno: {
+    x: -2087,
+    y: 3870,
+    scale: 4.9,
+    map: deinferno,
+  },
+  de_overpass: {
+    x: -4831,
+    y: 1781,
+    scale: 5.2,
+    map: deoverpass,
+  },
+  de_nuke: {
+    x: -3453,
+    y: 2887,
+    scale: 7,
+    map: denuke,
+  },
+  de_ancient: {
+    x: -2953,
+    y: 2164,
+    scale: 5,
+    map: deancient,
+  },
+  de_vertigo: {
+    x: -3168,
+    y: 1762,
+    scale: 4,
+    map: devertigo,
+  },
+  de_train: {
+    x: -2477,
+    y: 2392,
+    scale: 4.7,
+    map: detrain,
+  },
+  de_dust2: {
+    x: -2476,
+    y: 3239,
+    scale: 4.4,
+    rotate: 1,
+    zoom: 1.1,
+    map: dedust2,
+  },
+  de_cache: {
+    x: -2000,
+    y: 3250,
+    scale: 5.5,
+    map: decache,
   },
 };
